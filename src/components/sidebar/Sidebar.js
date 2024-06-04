6a3333ec83f655a8376a734bbc471fa5ad9b4201
irada <irada.mammadovaaa@gmail.com>
2024-06-04 12:10:11 +0400

    some little changes

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { ReactComponent as LogoWithTitle } from "assets/icons/spotifyIconWithTitle.svg";
+import { ReactComponent as Logo } from "assets/icons/logo.svg";
 import FooterSidebar from "components/footer-sidebar/FooterSidebar";
 
 import "./Sidebar.scss";
@@ -9,7 +9,7 @@ const Sidebar = () => {
   return (
     <div className="Sidebar">
       <p>Sidebar</p>
-      <LogoWithTitle className={"LogoWithTitle"} />
+      <Logo className={"Logo"} />
       <FooterSidebar />
     </div>
   );
