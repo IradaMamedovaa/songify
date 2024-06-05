b91001e91bddd253c1eabf0e79ec1c31a55baf33
irada <irada.mammadovaaa@gmail.com>
2024-06-05 16:48:20 +0400

    some design changes

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -8,13 +8,14 @@ import FooterSidebar from "components/footer-sidebar/FooterSidebar";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
-
-const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="Sidebar">
       <LogoHeadPhone className={"Logo"} />
 
-      <h1 onClick={() => navigate(ROUTE_NAMES.search)}>SEARCH</h1>
+      <h1 style={{maxWidth: "100px", marginRight: "100px", border: "1px solid white", padding: "20px" }}onClick={() => navigate(ROUTE_NAMES.search)}>
+        navigate to <i>search</i> page
+      </h1>
       <FooterSidebar />
     </div>
   );
