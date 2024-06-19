2b23a26e22ff23dc6fd81da9d9af8afa1d8d4f43
irada <irada.mammadovaaa@gmail.com>
2024-06-19 16:13:52 +0400

    added font, renamed again lol

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { ROUTE_NAMES } from "utils/constants/routes";
 
-import { ReactComponent as LogoHeadPhone } from "assets/icons/headphone.svg";
+import LogoHeadPhone from "assets/icons/favicon.png";
 import FooterSidebar from "components/footer-sidebar/FooterSidebar";
 
 import "./Sidebar.scss";
@@ -11,9 +11,20 @@ const Sidebar = () => {
   const navigate = useNavigate();
   return (
     <div className="Sidebar">
-      <LogoHeadPhone className={"Logo"} />
+      <div className="LogoWrapper">
+        <img src={LogoHeadPhone} className={"Logo"} alt={'logo'} />
+        <p className="LogoName">vista</p>
+      </div>
 
-      <h1 style={{maxWidth: "100px", marginRight: "100px", border: "1px solid white", padding: "20px" }}onClick={() => navigate(ROUTE_NAMES.search)}>
+      <h1
+        style={{
+          maxWidth: "100px",
+          marginRight: "100px",
+          border: "1px solid white",
+          padding: "20px",
+        }}
+        onClick={() => navigate(ROUTE_NAMES.search)}
+      >
         navigate to <i>search</i> page
       </h1>
       <FooterSidebar />
